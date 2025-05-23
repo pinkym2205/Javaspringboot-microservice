1dc58333c8affcac9f8bd00dc02a0f66a997dbcc
Pinky Mahesh <pinkym2202@gmail.comgit config --global user.email pinkym2205@gmail.com>
2025-05-23 20:22:12 +0530

    Initial commit - Spring Boot project setup

---
+++ currency-exchange-sample-service/src/main/java/com/example/currency_exchange_sample_service/CurrencyExchangeSampleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_exchange_sample_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeSampleServiceApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-sample-service/src/test/java/com/example/currency_exchange_sample_service/CurrencyExchangeSampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currency_exchange_sample_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
